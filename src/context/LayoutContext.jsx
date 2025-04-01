a163f69932409db58479f21df5da50fdf2261fe7
vijin <vijinrinz101@gmail.com>
2025-04-01 23:48:11 +0530

    dashbord added

--- src/context/LayoutContext.jsx
+++ src/context/LayoutContext.jsx
@@ -1,6 +1,6 @@
 "use client";
 import { createContext, useContext, useMemo, useState } from "react";
-import CustomSnackbar from "../components/Notification/CustomSnackbar";
+import CustomSnackbar from "../Components/Notification/CustomSnackbar";
 
 export const LayoutContext = createContext();
 
